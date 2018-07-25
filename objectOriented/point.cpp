4664ae01ad9d2ce73824569811e4c619acc9621c
kotharisonal <kotharisonal@gmail.com>
2018-07-25 14:16:58 -0700

    Create point.cpp

---
+++ objectOriented/point.cpp
@@ -0,0 +1,18 @@
+public class Point {
+  private int x;
+  private int y;
+  public Point(int x, int y){
+    this.x = x;
+    this.y = y;
+  }
+  public Point(Point other){
+    x = other.getX();
+    y = other.getY();
+  }
+  public int getX(){return x;}
+  public int getY(){return y;}
+  public Point relativeTo(int dx, int dy){
+    return new Point(x+dx, y+dy);
+  }
+  
+  
