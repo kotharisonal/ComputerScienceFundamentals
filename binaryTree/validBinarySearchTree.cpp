b900620a0d46a618b532b01e9f2203b155e0d996
kotharisonal <kotharisonal@gmail.com>
2018-08-15 20:45:49 -0700

    Update validBinarySearchTree.cpp

--- binaryTree/validBinarySearchTree.cpp
+++ binaryTree/validBinarySearchTree.cpp
@@ -31,3 +31,22 @@ public:
     }
 };
 
+
+   public boolean isBST(Node root){
+       return isBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+   }
+
+   private boolean isBST(Node root, int min, int max){
+       
+       if(root == null){
+           return true;
+       }
+
+       if(root.data <= min || root.data > max){
+           return false;
+       }
+
+       return isBST(root.left, min, root.data) && isBST(root.right, root.data, max);
+
+   }
+
